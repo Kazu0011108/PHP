353190cc488e7443b462156125b2786eaec9dfde
Kazu0011108 <takanomeru53541181108@gmail.com>
2022-01-25 17:20:55 +0900

    step5

--- php/restaurants_searcher.php
+++ php/restaurants_searcher.php
@@ -3,6 +3,10 @@ function write_data_to_csv(){
  $restaurants = [];
  $response = "hugahuga";
 
+ if(isset($response["results"]["error"])){
+     return print("エラーが発生しました!")
+ }
+
  return print_r($restaurants);
 }
 
